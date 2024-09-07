185f9b66b59d0c8dd40820b7005f132b862ddaf5
unknown <kenjy.moura@gmail.com>
2024-09-07 19:03:53 -0400

    Projeto

---
+++ projeto.js
@@ -0,0 +1,16 @@
+function trocar() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // pegar a tag img
+  const img = document.querySelector(".foto-perfil")
+
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "../Imagens/ARTE-4.png")
+  } else {
+    // set tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "../Imagens/ARTE-5.png")
+  }
+}
